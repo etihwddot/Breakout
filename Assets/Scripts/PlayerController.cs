543f0a9bb2e2e8492d1e33509cbc79024332215b
Todd White <twhite@logos.com>
2015-03-14 21:14:23 -0700

    Add simple script to move player.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float Speed;
+
+	void Update ()
+	{
+		var moveHorizontal = Input.GetAxis("Horizontal");
+		GetComponent<Rigidbody>().velocity = new Vector3(moveHorizontal, 0, 0) * Speed;
+	}
+}
