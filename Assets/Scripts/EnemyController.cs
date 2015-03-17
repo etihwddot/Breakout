419af0d6bc560a65a02b7444a55d6e0c457054b2
Todd White <todd.white@logos.com>
2015-03-17 13:01:16 -0700

    Add first enemy.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour
+{
+	public int HitPoints = 1;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (!other.CompareTag("Ball"))
+			return;
+
+		HitPoints--;
+
+		if (HitPoints == 0)
+			Destroy(gameObject);
+	}
+}
